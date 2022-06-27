56b70b8afc2fb1875b4fd1930dd99290bd068fef
kmes9940502 <kmes9940502@gmail.com>
2022-06-28 02:06:51 +0800

    finder.sh finished

---
+++ finder-app/finder.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+filesdir=$1
+searchstr=$2
+counter=0
+strcounter=0
+
+#check if the parameters is valid
+if [ $# -lt 2 ]; then
+    echo "Parameters not specified!!"
+    exit 1
+elif [ $# -gt 2 ]; then
+    echo "Too much parameters!!"
+    exit 1
+else
+    #check if the directory exist
+    if [ -d "$filesdir" ]; then
+        echo "Path: ${filesdir}, searching key: ${searchstr}"
+    else
+        echo "Directory does not exists!!"
+        exit 2
+    fi
+fi
+
+
+#execution start here
+
+for i in $(find "$filesdir" -type f)
+do
+    counter=`expr $counter + 1`
+done
+
+keyLine="grep -r ${searchstr} ${filesdir}"
+
+for j in $(${keyLine})
+do
+    strcounter=`expr $strcounter + 1`
+done
+echo "The number of files are ${counter} and the number of matching lines are ${strcounter}"
+exit 0
